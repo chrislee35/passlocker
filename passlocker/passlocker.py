7a0dcf4f2ff0e182631a2362788da37817b09c96
chris <chris@oxygen>
2020-12-03 14:26:57 -0500

    improved handling of accounts that have spaces in them; added a renaming tool

--- passlocker/passlocker.py
+++ passlocker/passlocker.py
@@ -244,7 +244,7 @@ class PassLocker:
         json.dump(master, fh)
         fh.close()
         
-    def list_accounts(self):
+    def list_accounts(self, sep=' '):
         #s1 = time.time()
         # listdir is 3 times faster than glob (0.4 ms vs. 1.2 ms for 461 files)
         # timing still heavily dependent on os file caching
@@ -256,7 +256,7 @@ class PassLocker:
         #s3 = time.time()
         b64decoded = [ b64d(x) for x in b64strs ]
         #s4 = time.time()
-        accounts = [ x.decode('UTF-8').replace('|', ' ', 1).encode('UTF-8') for x in b64decoded ]
+        accounts = [ x.decode('UTF-8').replace('|', sep, 1).encode('UTF-8') for x in b64decoded ]
         #s5 = time.time()
         #print("list files: %0.2f, b64str: %0.2f, b64decode: %0.2f, format: %0.2f" % ((s2 - s1)*1000, (s3 - s2)*1000, (s4 - s3)*1000, (s5 - s4)*1000))
         
