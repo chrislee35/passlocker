a8d57f549498dce143f8bfb2f2001371808ee740
Your Name <rubygems@chrislee.dhs.org>
2024-12-20 09:59:12 -0500

    updating build system

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 
 from distutils.core import setup
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-    
 setup(name='passlocker',
-      version='0.0.9',
-      description='Password Encryption',
-      author='Chris Lee',
-      author_email='python@chrisleephd.us',
-      url='https://github.com/chrislee35/passlocker',
-      packages=['passlocker'],
-      scripts=['bin/passlocker', 'bin/passlocker_cui', 'bin/passlocker_rename'],
-      install_requires=requirements,
-      classifiers=[
-              "Development Status :: 3 - Alpha",
-              "Topic :'': Utilities",
-              "License :: OSI Approved :: MIT License",
-          ],
-      
-     )
+    version='0.0.9',
+    description='Password Encryption',
+    author='Chris Lee',
+    author_email='python@chrisleephd.us',
+    url='https://github.com/chrislee35/passlocker',
+    packages=['passlocker'],
+    scripts=['bin/passlocker', 'bin/passlocker_cui', 'bin/passlocker_rename'],
+    install_requires=[
+        "pycryptodome",
+        "pyperclip",
+        "pwinput"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :'': Utilities",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
